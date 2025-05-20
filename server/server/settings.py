c50e0b26186261da8f676ed94109e74a542d383b
WOLF166 <97960687+WOLF166@users.noreply.github.com>
2025-05-20 18:48:00 +0300

    fix_p3_1

--- server/server/settings.py
+++ server/server/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+REACTPY_STATIC_ROOT = os.path.join(STATIC_ROOT, 'reactpy_django')
+if not os.path.exists(REACTPY_STATIC_ROOT):
+    os.makedirs(REACTPY_STATIC_ROOT)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
